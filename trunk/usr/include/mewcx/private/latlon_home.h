659f7448792f1bcec9cf0dc728b9e08a58919dfc
mike.weiblen@gmail.com <mike.weiblen@gmail.com@cdcf2cee-3461-b1ea-8268-8f29bb7d09f8>
2011-04-17 20:40:32 +0000

    add generic home latlon

---
+++ trunk/usr/include/mewcx/private/latlon_home.h
@@ -0,0 +1,14 @@
+// usr/include/private/latlon_home.h
+// http://mew.cx/
+
+#ifndef LATLON_HOME_H
+#define LATLON_HOME_H
+
+#define HOME_LONGITUDE  (-106.00000)    // [degrees longitude]
+#define HOME_LATITUDE   (39.00000)      // [degrees latitude]
+#define HOME_ELEVATION  (   0.00)       // [meters ASL] 8810 feet
+#define HOME_UTM_ZONE   (13)
+
+#endif
+
+// vim: set sw=4 ts=8 et ic ai:
